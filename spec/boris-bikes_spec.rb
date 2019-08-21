88e73357a38f90b1fe9acbaaa5d4b6063d754085
Mike <mjwalshe@gmail.com>
2019-08-21 22:49:07 +0100

    Refactor tests

--- spec/boris-bikes_spec.rb
+++ spec/boris-bikes_spec.rb
@@ -1,48 +1,51 @@
 require './lib/docking_station'
 
-describe "Docking Station class" do
-    describe DockingStation do
-      describe "creating a new DockingStation" do
-        it "creates a new docking station object" do
-          expect(DockingStation.new).to be_instance_of DockingStation
-        end
-      end
+describe DockingStation do
+  describe "creating a new DockingStation" do
+    it "creates a new docking station object" do
+      expect(DockingStation.new).to be_instance_of DockingStation
+    end
+  end
 
-      describe "#full?" do
-        it "checks whether station is full" do
-          true
-        end
-      end
+  describe "#full?" do
+    it "checks whether station is full" do
+      true
+    end
+  end
 
-      describe "#dock_bike" do
-        it "returns docked bike" do
-          bike_1 = Bike.new
-          subject.dock_bike(bike_1)
-          expect(subject.bike).to eq bike_1
-        end
-      end
+  describe "#dock_bike" do
+    it "returns docked bike" do
+      bike_1 = Bike.new
+      subject.dock_bike(bike_1)
+      expect(subject.bike).to eq bike_1
+    end
+  end
 end
 
-  describe DockingStation do
-    describe "#release_bike" do
-      it "releases an existing bike" do
-        station = DockingStation.new
-        expect(station.release_bike).to eq (:bike)
-      end
+describe DockingStation do
+  describe "#release_bike" do
+    it "releases an existing bike" do
+      station = DockingStation.new
+      expect(station.release_bike).to eq (:bike)
     end
+  end
 
-    describe  "#release_bike" do
-      it "raises an error when there are no bikes available" do
-        station = DockingStation.new
-        expect { station.release_bike }.to raise_error 'No bikes available'
-      end
-   end
- end
-
-    describe DockingStation do
-      it { should respond_to(:release_bike)}
+  describe  "#release_bike" do
+    it "raises an error when there are no bikes available" do
+      station = DockingStation.new
+      expect { station.release_bike }.to raise_error 'No bikes available'
     end
+  end
 end
+
+describe DockingStation do
+  it { should respond_to(:release_bike) }
+end
+
+
+
+
+
 require './lib/bike'
 
   describe "Bike class" do
