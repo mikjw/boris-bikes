f56bac1a2941515b323614e93184f35d7f71a25b
Mike <mjwalshe@gmail.com>
2019-08-21 15:00:05 +0100

    Add tests for release_bike method and Bike class

--- spec/boris-bikes_spec.rb
+++ spec/boris-bikes_spec.rb
@@ -5,8 +5,23 @@ describe "Docking Station class" do
     describe "creating a new DockingStation" do
       it "creates a new docking station object" do
         expect(DockingStation.new).to be_instance_of DockingStation
-      end 
-    end 
-  end 
+      end
+    end
+  end
+end
+
+describe DockingStation do
+  it { should respond_to(:release_bike)}
 end 
 
+describe Bike do 
+  it { should be_instance_of Bike }
+end 
+
+describe "#working?" do
+  it "bike is working" do
+    bike_1 = Bike.new
+    expect(bike_1).to respond_to(:working?)
+  end 
+end 
+    
